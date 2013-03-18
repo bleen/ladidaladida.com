b7d3f70335d5490960121251cb05a86038bd142a
bleen <git.119x@bleen.net>
2013-03-17 20:54:12 -0400

    Adding a facebook like button

--- sites/all/themes/ladida/page.tpl.php
+++ sites/all/themes/ladida/page.tpl.php
@@ -12,6 +12,15 @@
   </head>
 
   <body>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
     <div id="admin"><?php print $admin; ?></div>
 
     <div id="wrapper">
@@ -31,6 +40,7 @@
       </div>
 
       <div id="content">
+        <div class="fb-like-box" data-href="http://www.facebook.com/pages/LaDiDa/131956486829974" data-width="175" data-show-faces="false" data-stream="false" data-header="true"></div>
         <?php if ($tabs): print $tabs; endif; ?>
         <?php if ($tabs2): print $tabs2; endif; ?>
         <?php if ($show_messages && $messages){ print $messages; } ?>
